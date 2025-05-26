070ebda4aa62732dfd2f6c3c6c3338ec02452d41
Daniel_jc <jcxma@outlook.com>
2025-05-26 15:05:10 -0400

    added  comments to card class

--- PokerGame/Card.py
+++ PokerGame/Card.py
@@ -1,10 +1,10 @@
 class Card:
    
- def __init__(self, rank: str, suit: str):
+ def __init__(self, rank: str, suit: str): #This makes the card object
     self.rank = rank
     self.suit = suit
 
-def get_value(self) -> int:
+def get_value(self) -> int: #this creates a method to return the int value of the card 
         rank_values = {
            "A":1, "2":2, "3":3, "4":4, "5":5,
             "6":6, "7":7, "8":8, "9":9, "10":10,
@@ -12,7 +12,7 @@ def get_value(self) -> int:
         }
         return rank_values.get(self.rank,0)
    
-def get_suit_rank(self) -> int:
+def get_suit_rank(self) -> int: # creates a method for getting the int value of the suits
         suit_rank = {
              'C':1,
              'D':2,
@@ -22,11 +22,11 @@ def get_suit_rank(self) -> int:
         }
         return suit_ranks.get(self.suit, 0)
 
-def get_rank(self) ->str:
+def get_rank(self) ->str: # gets the rankinf of the card as a string
      return self.rank
 
-def get_suit(self) ->str:
+def get_suit(self) ->str: # gets the suit fo the card as a string
      return self.suit
 
-def __str__(self) -> str:
+def __str__(self) -> str:  # pretty much the toString like in java to replace the default print whne printed "raw" 
      return f"{self.rank}{self.suit}"
